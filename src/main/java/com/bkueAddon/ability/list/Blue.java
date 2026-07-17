2936fd20c9441d4bf1b6421ce124b738ea4b01da
bkuenewbie <bkuenewbie@gmail.com>
2026-07-17 18:27:14 +0900

    1.0.2

--- src/main/java/com/bkueAddon/ability/list/Blue.java
+++ src/main/java/com/bkueAddon/ability/list/Blue.java
@@ -17,13 +17,11 @@ import daybreak.abilitywar.utils.library.PotionEffects;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
-import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.PlayerDeathEvent;
-import org.bukkit.inventory.ItemStack;
 
 @AbilityManifest(name = "블루", rank = Rank.SPECIAL, species = Species.HUMAN, explain = {
         "§7패시브 §8- §b빙결 적응§f: 얼음 위에 있을 때 §6힘 I§f, §3저항 I§f, §b신속 I§f를 얻습니다.",
-        "- 얼음 위에서 공격하면 피해가 30% 증가하고 대상을 1초간 속박합니다.",
+        "- 얼음 위에서 공격하면 대상을 1초간 속박합니다.",
         "§7패시브 §8- §b영구 동토§f: 플레이어를 처치하면 해당 위치 주변 $[RANGE_CONFIG]칸이 얼음으로 변합니다.",
         "§7철괴 우클릭 §8- §b빙결 지대§f: 5초 동안 주변 $[RANGE_CONFIG]칸을 얼음으로 바꾸고 범위 내 적을 3초간 속박합니다. $[COOLDOWN_CONFIG]"
 }, summarize = {
@@ -99,7 +97,7 @@ public class Blue extends AbilityBase implements ActiveHandler {
                 Block below = player.getLocation().clone().subtract(0, 1, 0).getBlock();
 
                 if (snapshots.getBlocks().contains(below) && getGame().isParticipating(player) && !TeamUtil.isTeammate(getPlayer(), player)) {
-                    Rooted.apply(getGame().getParticipant(player), TimeUnit.SECONDS, 3);
+                    Rooted.apply(getGame().getParticipant(player), TimeUnit.SECONDS, 1);
                 }
             }
         }
@@ -117,7 +115,6 @@ public class Blue extends AbilityBase implements ActiveHandler {
     protected void onUpdate(Update update) {
         if (update == Update.RESTRICTION_CLEAR) {
             countdown.start();
-            getPlayer().getInventory().addItem(new ItemStack(Material.ICE, 64), new ItemStack(Material.ICE, 64));
         }
         if (update == Update.ABILITY_DESTROY) {
             permanentIce.restore();
@@ -132,22 +129,6 @@ public class Blue extends AbilityBase implements ActiveHandler {
             return true;
         }
         return false;
-    }
-
-    @SubscribeEvent
-    public void onEntityDamageByEntity(EntityDamageByEntityEvent e) {
-        if (!e.getDamager().equals(getPlayer())) return;
-        if (!(e.getEntity() instanceof Player)) return;
-        Material below = getPlayer().getLocation().clone().subtract(0, 1, 0).getBlock().getType();
-        if (below == Material.ICE) {
-            e.setDamage(e.getDamage() * 1.3);
-
-            Player victim = (Player) e.getEntity();
-
-            if (getGame().isParticipating(victim) && !TeamUtil.isTeammate(getPlayer(), victim)) {
-                Rooted.apply(getGame().getParticipant(victim), TimeUnit.SECONDS, 1);
-            }
-        }
     }
 
     @SubscribeEvent
