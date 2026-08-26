dec085ab9a6af262b3ccd3ce91f6eddb69414273
bkuenewbie <bkuenewbie@gmail.com>
2026-08-26 10:49:08 +0400

    1.0.8

--- src/main/java/com/bkueAddon/ability/list/EnchantRich.java
+++ src/main/java/com/bkueAddon/ability/list/EnchantRich.java
@@ -16,11 +16,11 @@ import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 
 @AbilityManifest(name = "인챈트 부자", rank = Rank.L, species = Species.HUMAN, explain = {
-        "§7패시브 §8- §e황금의 손길§f: 게임 시작 시 가지고 있는 아이템을 포함하여, 인벤토리에 들어오는 모든 장비와 무기에 자동으로 최고의 인챈트가 부여됩니다.",
+        "§7패시브 §8- §e황금의 손길§f: 게임 시작 시 가지고 있는 아이템을 포함하여, 인벤토리에 들어오는 모든 장비와 무기의 인챈트를 제거하고 최고의 인챈트를 부여합니다.",
         "- §b[보호 IV / 내구성 III / 날카로움 V / 발화 II]",
         "§7아이디어 제공 §8- §6goodhyojun"
 }, summarize = {
-        "획득하는 모든 장비와 무기에 자동으로 최고 수준의 인챈트를 부여하는 패시브 능력입니다."
+        "장비와 무기의 인챈트를 제거하고 최고의 인챈트를 부여합니다."
 })
 public class EnchantRich extends AbilityBase {
 
@@ -35,39 +35,64 @@ public class EnchantRich extends AbilityBase {
         ItemMeta meta = item.getItemMeta();
         if (meta == null) return false;
 
-        boolean equipment = type.name().endsWith("_HELMET") || type.name().endsWith("_CHESTPLATE") || type.name().endsWith("_LEGGINGS") || type.name().endsWith("_BOOTS");
-        boolean weapon = type.name().endsWith("_SWORD") || type.name().endsWith("_AXE");
+        boolean equipment = type.name().endsWith("_HELMET")
+                || type.name().endsWith("_CHESTPLATE")
+                || type.name().endsWith("_LEGGINGS")
+                || type.name().endsWith("_BOOTS");
 
-        if (equipment) meta.addEnchant(Enchantment.PROTECTION_ENVIRONMENTAL, 4, true);
+        boolean weapon = type.name().endsWith("_SWORD")
+                || type.name().endsWith("_AXE");
+
+        if (!equipment && !weapon && type.getMaxDurability() <= 0) return false;
+        for (Enchantment enchantment : meta.getEnchants().keySet()) {
+            meta.removeEnchant(enchantment);
+        }
+
+        if (equipment) {
+            meta.addEnchant(Enchantment.PROTECTION_ENVIRONMENTAL, 4, true);
+        }
+
         if (weapon) {
             meta.addEnchant(Enchantment.DAMAGE_ALL, 5, true);
             meta.addEnchant(Enchantment.FIRE_ASPECT, 2, true);
         }
-        if (type.getMaxDurability() > 0) meta.addEnchant(Enchantment.DURABILITY, 3, true);
+        
+        if (type.getMaxDurability() > 0) {
+            meta.addEnchant(Enchantment.DURABILITY, 3, true);
+        }
 
         item.setItemMeta(meta);
-        return equipment || weapon || type.getMaxDurability() > 0;
+        return true;
     }
 
     @Override
     protected void onUpdate(Update update) {
         if (update == Update.RESTRICTION_CLEAR) {
-            for (ItemStack item : getPlayer().getInventory().getContents()) applyEnchants(item);
-            ParticleLib.ENCHANTMENT_TABLE.spawnParticle(getPlayer().getLocation().add(0, 1, 0), 0.5f, 0.8f, 0.5f, 20, 0.1);
+            for (ItemStack item : getPlayer().getInventory().getContents()) {
+                applyEnchants(item);
+            }
+
+            ParticleLib.ENCHANTMENT_TABLE.spawnParticle(
+                    getPlayer().getLocation().add(0, 1, 0),
+                    0.5f, 0.8f, 0.5f, 20, 0.1
+            );
+
             SoundLib.BLOCK_ENCHANTMENT_TABLE_USE.playSound(getPlayer(), 1, 1);
         }
     }
 
     @SubscribeEvent
     public void onEntityPickupItem(EntityPickupItemEvent event) {
-        if (event.getEntity().equals(getPlayer())) applyEnchants(event.getItem().getItemStack());
+        if (event.getEntity().equals(getPlayer())) {
+            applyEnchants(event.getItem().getItemStack());
+        }
     }
 
     @SubscribeEvent
     public void onInventoryClick(InventoryClickEvent event) {
         if (event.getWhoClicked().equals(getPlayer())) {
             if (applyEnchants(event.getCurrentItem()) || applyEnchants(event.getCursor())) {
-                ((org.bukkit.entity.Player) event.getWhoClicked()).updateInventory();
+                getPlayer().updateInventory();
             }
         }
     }
@@ -75,7 +100,9 @@ public class EnchantRich extends AbilityBase {
     @SubscribeEvent
     public void onInventoryOpen(InventoryOpenEvent event) {
         if (event.getPlayer().equals(getPlayer())) {
-            for (ItemStack item : event.getInventory().getContents()) applyEnchants(item);
+            for (ItemStack item : event.getInventory().getContents()) {
+                applyEnchants(item);
+            }
         }
     }
 }
--- src/main/java/com/bkueAddon/ability/list/Immortal.java
+++ src/main/java/com/bkueAddon/ability/list/Immortal.java
@@ -3,45 +3,31 @@ package com.bkueAddon.ability.list;
 import daybreak.abilitywar.ability.AbilityBase;
 import daybreak.abilitywar.ability.AbilityManifest;
 import daybreak.abilitywar.ability.AbilityManifest.*;
+import daybreak.abilitywar.ability.Materials;
 import daybreak.abilitywar.ability.SubscribeEvent;
 import daybreak.abilitywar.game.AbstractGame.Participant;
 import daybreak.abilitywar.utils.library.ParticleLib;
 import daybreak.abilitywar.utils.library.SoundLib;
+import org.bukkit.Material;
 import org.bukkit.event.entity.EntityDamageEvent;
-import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
 
 @AbilityManifest(name = "이모탈", rank = Rank.A, species = Species.OTHERS, explain = {
-        "§7패시브 §8- §b불멸§f: 치명적인 피해를 받으면 한 번 죽음을 무시하고 부활합니다.",
-        "- 부활 후 §a재생 II§f와 §b흡수 II§f를 얻습니다."
+        "§7패시브 §8- §b불멸§f: 게임 시작 시 §e불사의 토템§f을 지급받습니다."
 }, summarize = {
-        "한 번 죽음을 무시하고 부활합니다."
+        "불사의 토템을 지급받습니다."
 })
 public class Immortal extends AbilityBase {
-
-    private boolean revived = false;
-
     public Immortal(Participant participant) {
         super(participant);
     }
 
-    @SubscribeEvent
-    public void onDamage(EntityDamageEvent e) {
-        if (!e.getEntity().equals(getPlayer()) || revived) return;
-
-        if (e.getFinalDamage() >= getPlayer().getHealth()) {
-            revived = true;
-            e.setCancelled(true);
-
-            getPlayer().setHealth(Math.min(10.0, getPlayer().getMaxHealth()));
-            getPlayer().addPotionEffect(new PotionEffect(PotionEffectType.REGENERATION, 100, 1));
-            getPlayer().addPotionEffect(new PotionEffect(PotionEffectType.ABSORPTION, 100, 1));
-
-            SoundLib.ITEM_TOTEM_USE.playSound(getPlayer(), 1, 1);
-            ParticleLib.TOTEM.spawnParticle(getPlayer().getLocation().add(0, 1, 0), 0.5, 0.8, 0.5, 30, 0.1);
-
-            getPlayer().sendMessage("§a§l[이모탈] §f죽음을 극복했습니다!");
+    @Override
+    protected void onUpdate(Update update) {
+        if (update == Update.RESTRICTION_CLEAR) {
+            getPlayer().getInventory().addItem(new ItemStack(Material.TOTEM_OF_UNDYING));
         }
     }
 }
